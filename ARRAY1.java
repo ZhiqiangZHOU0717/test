08b2cc3269d2fc5db67ed7b6f5d844e851278ce3
ZhiqiangZHOU0717 <74814918+ZhiqiangZHOU0717@users.noreply.github.com>
2020-11-21 18:32:03 +0800

    Add files via upload

---
+++ ARRAY1.java
@@ -0,0 +1,14 @@
+package cn.itcast.day05basic;
+
+public class ARRAY1 {
+    public static void main(String[] args) {
+        int[] zzq = {10, 20, 30, 40, 50, 60};
+        int a = 0;
+        for (int i = 0; i < zzq.length; i++) {
+
+            System.out.println(a);
+        }
+
+    }
+}
+
